5c8bea9bb371ff11fb626df7a71c90d40c983e15
Artur Minasjan <>
2024-05-27 19:50:36 +0200

    copyright added

--- src/components/Table/Legend.jsx
+++ src/components/Table/Legend.jsx
@@ -1,12 +1,14 @@
 import "./table.css";
 import LegendBody from "./LegendBody";
 import LegendHead from "./LegendHead";
+import LegendFooter from "./LegendFooter";
 
 export default function Legend() {
   return (
     <table id="periodontalTable">
       <LegendHead />
       <LegendBody />
+      <LegendFooter />
     </table>
   );
 }
---
+++ src/components/Table/LegendFooter.jsx
@@ -0,0 +1,11 @@
+export default function LegendFooter() {
+  return (
+    <tfoot>
+      <tr>
+        <th colspan="9">
+          <p>Copyright (c) 2024 www.perioimplantchart.com</p>
+        </th>
+      </tr>
+    </tfoot>
+  );
+}
