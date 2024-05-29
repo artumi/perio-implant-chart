5fc35557b8dd594c93577c32566de2092f1c9772
Artur Minasjan <>
2024-05-29 21:36:27 +0200

    mucosa full chart

--- src/components/Table/LegendBodyMucosa.jsx
+++ src/components/Table/LegendBodyMucosa.jsx
@@ -4,6 +4,8 @@ import InputRow from "./Rows/InputRow";
 import CheckBoxRow from "./Rows/CheckBoxRow";
 import NumberRow from "./Rows/NumberRow";
 import ToothUpRow from "./Rows/ToothUpRow";
+import DividerRow from "./Rows/DividerRow";
+import ToothDownRow from "./Rows/ToothDownRow";
 
 export default function LegendBodyMucosa() {
   return (
@@ -21,6 +23,20 @@ export default function LegendBodyMucosa() {
       <NumberRow title="MT" />
       <CheckBoxRow title="STM" />
       <CheckBoxRow title="PMC" />
+      <DividerRow />
+      <CheckBoxRow title="PMC" />
+      <CheckBoxRow title="STM" />
+      <NumberRow title="MT" />
+      <NumberRow title="KMW" />
+      <NumberRow title="STH" />
+      <ToothDownRow />
+      <NumberRow title="STH" />
+      <NumberRow title="KMW" />
+      <NumberRow title="MT" />
+      <CheckBoxRow title="STM" />
+      <CheckBoxRow title="PMC" />
+      <InputRow title="Fixation" />
+      <NotesRow />
     </tbody>
   );
 }
